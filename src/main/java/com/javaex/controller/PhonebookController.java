ef61b32a706a91ddce446cb7e871df174b29079a
Yuchan Kim <yuchankim0906@gmail.com>
2024-09-23 17:54:47 +0900

    updates

--- src/main/java/com/javaex/controller/PhonebookController.java
+++ src/main/java/com/javaex/controller/PhonebookController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.javaex.service.PhonebookService;
@@ -51,6 +51,37 @@ public class PhonebookController {
 			return JsonResult.success(count);
 		}
 		
+	}
+	
+	@GetMapping(value="/api/persons/{no}")
+	public JsonResult getPerson(@PathVariable(value="no") int no) {
+		System.out.println("Phonebookcontroller.getPerson()");
+		System.out.println(no);
+		
+		PersonVo personVo = phonebookService.exeEditForm(no);
+		
+		
+		if (personVo == null) {
+			return JsonResult.fail("번호가 없습니다.");
+		}else {
+			return JsonResult.success(personVo);
+		}
+	}
+	
+	@PutMapping(value = "api/persons/{no}")
+	public JsonResult updatePerson(@PathVariable(value = "no") int personId, @RequestBody PersonVo personVo) {
+		System.out.println("PhonebookController.updatePerson()");
+		System.out.println(personVo); //Axios에서 data값은 화면에 놓이기 때문에 RequestBody로 가져온다.
+		personVo.setPersonId(personId);
+		System.out.println(personVo);
+		
+		int count = phonebookService.exeEditPerson(personVo);
+		System.out.println(count);
+		if (count == 0) {
+			return JsonResult.fail("수정할 데이터가 없습니다.");
+		}else {
+			return JsonResult.success("수정 완료.");
+		}
 	}
 	
 }
